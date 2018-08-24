33602bc0620ad4b503e06e9a893a814dddb29c3d
Yanuar <beep4685@gmail.com>
2018-08-24 15:41:02 +0700

    add image

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,6 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Image source={require('.//image/pic.jpg')} 
+         style={{width: 400, height: 400}} />
         <Text style={styles.instructions}>Nama : M Yanuar Budi F</Text>
         <Text style={styles.instructions}>Kelas : XI RPL 1</Text>
         <Text style={styles.instructions}>Absen : 24</Text>
